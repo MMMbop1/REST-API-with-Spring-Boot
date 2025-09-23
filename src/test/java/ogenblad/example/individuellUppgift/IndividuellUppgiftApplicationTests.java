029071651f04621f8d24eb18bc243eaa758449a2
unknown <ludvigkaskogenblad@gmail.com>
2025-09-23 18:45:50 +0200

    initial commit

---
+++ src/test/java/ogenblad/example/individuellUppgift/IndividuellUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package ogenblad.example.individuellUppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividuellUppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
